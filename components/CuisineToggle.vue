06d4d6753299ea9fd33dc47af7c05f91255a399c
caelwarner <caelawarner@gmail.com>
2024-05-18 23:24:47 -0700

    Added CuisineToggle

---
+++ components/CuisineToggle.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import {Toggle} from "~/components/ui/toggle";
+
+const model = defineModel<boolean>();
+const props = defineProps<{ name: string }>();
+</script>
+
+<template>
+    <Toggle v-model:pressed="model" variant="outline" class="w-36 h-36 flex flex-col" :class="[model ? 'border-primary' : '']">
+        <slot />
+        <p class="mb-4 font-bold">{{ props.name }}</p>
+    </Toggle>
+</template>
+
+<style scoped>
+
+</style>
